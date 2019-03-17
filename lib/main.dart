6302da7c0ac228c59800c927b569a8ef735a8b99
Grégori Sória <gregori.soria@base.digital>
2019-03-17 01:15:30 -0300

    flutter init

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const trendingUrl = "https://api.giphy.com/v1/gifs/trending?api_key=Mg3vtzN0RXJbMDVFap9V1PCVDKBF5EcU&limit=25&rating=G";
+const searchUrl = "https://api.giphy.com/v1/gifs/search?api_key=Mg3vtzN0RXJbMDVFap9V1PCVDKBF5EcU&q=dogs&limit=25&offset=25&rating=G&lang=en";
+
+void main() => runApp(MaterialApp());
